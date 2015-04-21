984744fe79c7e6df142b6addc6527511b72f124d
Wu Zheng <wuzheng68@hotmail.com>
2015-04-21 22:03:20 +0800

    Add hero animation.

--- src/app/scenes/GameScene.lua
+++ src/app/scenes/GameScene.lua
@@ -2,16 +2,18 @@
 -- Author: Jacky Woo
 -- Date: 2015-04-20 23:58:09
 --
+local Hero = import( "../views/Hero" )
+
 local GameScene = class( "GameScene", function()
 	return display.newScene( "GameScene" )
 end )
 
 function GameScene:ctor()
-	-- body
+	
 end
 
 function GameScene:onEnter( ... )
-	-- body
+	self._hero = Hero.new():pos( display.cx, display.cy ):addTo( self ):scale( 5 )
 end
 
 function GameScene:onExit( ... )
--- src/app/views/Hero.lua
+++ src/app/views/Hero.lua
@@ -3,25 +3,58 @@
 -- Date: 2015-04-21 00:13:56
 --
 local Hero = class( "Hero", function()
-	return display.newSprite()
+	return display.newSprite():setNodeEventEnabled( true )
 end )
 
+Hero.Run_Anim_Name = "RUN_ANIM"
+Hero.Attack_Anim_Name = "ATTACK_ANIM"
+
 function Hero:ctor( ... )
-	-- body
+	self:attackOnce()
+end
+
+local sharedTextureCache     = cc.Director:getInstance():getTextureCache()
+
+local function newFrames( pattern, begin, length )
+	local frames = {}
+	local step = 1
+	local last = begin + length - 1
+	for index = begin, last, step do
+		local frameName = string.format( pattern, index )
+		local texture = sharedTextureCache:addImage( frameName )
+		local size = texture:getContentSize()
+		local frame = cc.SpriteFrame:create( frameName, cc.rect( 0, 0, size.width, size.height ) )
+		if not frames then
+			printError("Hero - newFrames() - invalid frame, name %s", tostring(frameName))
+            return
+		end
+		frames[ #frames + 1 ] = frame
+	end
+	return frames
 end
 
 function Hero:onEnter( ... )
 	-- 预加载所有动画并缓存
+	local runFrames = newFrames( "images/hero/run%d.png", 1, 8 )
+	local runAnim = display.newAnimation( runFrames, 0.5 / 8 )
+	display.setAnimationCache( Hero.Run_Anim_Name, runAnim )
+
+	self:run()
+
+	local attackFrames = newFrames( "images/hero/atk%d.png", 1, 4 )
+	local attackAnim = display.newAnimation( attackFrames, 0.2 / 4 )
+	display.setAnimationCache( Hero.Attack_Anim_Name, attackAnim )
 end
 
 function Hero:onExit( ... )
 	-- body
 end
 
-function Hero:attackOnce( ... )
-	local frames = display.newFrames( "images/hero/atk%d.png", 1, 4 )
-	local animation = display.newAnimation( frames, 0.2 / 4 )
-	self:playAnimationOnce( animation, false, function() end )
+function Hero:attackOnce()
+	local animation = display.getAnimationCache( Hero.Attack_Anim_Name )
+	if animation then
+		self:playAnimationOnce( animation, false, function() end )
+	end
 end
 
 function Hero:attacking( ... )
@@ -29,7 +62,10 @@ function Hero:attacking( ... )
 end
 
 function Hero:run( ... )
-	-- body
+	local animation = display.getAnimationCache( Hero.Run_Anim_Name )
+	if animation then
+		self:playAnimationForever( animation )
+	end
 end
 
 return Hero
