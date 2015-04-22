1ca9016fb85f8bd378c33743a14a9721bbcdb951
Wu Zheng <wuzheng68@hotmail.com>
2015-04-23 01:43:08 +0800

    Create map.

--- src/app/scenes/GameScene.lua
+++ src/app/scenes/GameScene.lua
@@ -2,18 +2,29 @@
 -- Author: Jacky Woo
 -- Date: 2015-04-20 23:58:09
 --
-local Hero = import( "../views/Hero" )
+local Hero = import( "..views.Hero" )
+local Map = import( "..views.Map" )
 
 local GameScene = class( "GameScene", function()
 	return display.newScene( "GameScene" )
 end )
 
-function GameScene:ctor()
-	
+function GameScene:ctor( level )
+	self._level = level or 1
 end
 
 function GameScene:onEnter( ... )
 	self._hero = Hero.new():pos( display.cx, display.cy ):addTo( self ):scale( 5 )
+
+	self._map = Map.new( {} ):addTo( self )
+
+	self:performWithDelay(function()
+		self._layer = display.newLayer():addTo( self )
+		self._layer:setKeypadEnabled( true )
+		self._layer:addNodeEventListener( cc.KEYPAD_EVENT, function(event)
+			printInfo("code = %s, key = %s", event.code, event.key)
+		end )
+	end, 0.5)
 end
 
 function GameScene:onExit( ... )
